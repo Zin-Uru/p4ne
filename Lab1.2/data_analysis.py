d1f33589e4f5baecf976c95dfbb769dd5703a30a
Zin-Uru <sacro.uni@gmail.com>
2022-01-18 18:23:22 +0300

    First plot

---
+++ Lab1.2/data_analysis.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+from matplotlib import pyplot as plot
+from openpyxl import load_workbook as xls
+
+wb = xls('data_analysis_lab.xlsx')
+sheet = wb['Data']
+
+
+def gv(x):
+    return x.value
+
+
+y = list(map(gv, sheet['A'][1:]))
+t = list(map(gv, sheet['C'][1:]))
+a = list(map(gv, sheet['D'][1:]))
+
+plot.plot(y, t, label='Температура')
+plot.plot(y, a, label='Активность Солнца')
+
+plot.show()
