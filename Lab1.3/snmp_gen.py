d8712b2d9d370a764c3f648f4f8d52c86fefe5bc
Zin-Uru <sacro.uni@gmail.com>
2022-01-19 20:15:40 +0300

    Add some comments

--- Lab1.3/snmp_gen.py
+++ Lab1.3/snmp_gen.py
@@ -15,13 +15,19 @@ n = nextCmd(SnmpEngine(),
             ObjectType(ObjectIdentity('1.3.6.1.2.1.2.2.1.2')),
             lexicographicMode=False)
 
+# посл-ность кортежей (errorIndication, errorStatus, errorIndex, varBinds)
+
 print(type(g))
 print(type(n))
 
+# итерируем генератор-последовательность
 for r in g:
+    # итерируем кортеж varBinds
     for e in r[3]:
         print(e)
 
+# итерируем генератор-последовательность
 for r in n:
+    # итерируем кортеж varBinds
     for e in r[3]:
         print(e)
