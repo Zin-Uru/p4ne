a7e59d3926f4708dfd5ef2c060f6f636601d46c0
Zin-Uru <sacro.uni@gmail.com>
2022-01-19 19:00:56 +0300

    SNMP

---
+++ Lab1.3/snmp_gen.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+from pysnmp.hlapi import *
+
+g = getCmd(SnmpEngine(),
+           CommunityData('public', mpModel=0),
+           UdpTransportTarget(('10.31.70.107', 161)),
+           ContextData(),
+           ObjectType(ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0)))
+
+n = nextCmd(SnmpEngine(),
+            CommunityData('public', mpModel=0),
+            UdpTransportTarget(('10.31.70.107', 161)),
+            ContextData(),
+            ObjectType(ObjectIdentity('1.3.6.1.2.1.2.2.1.2')),
+            lexicographicMode=False)
+
+for r in g:
+    for e in r[3]:
+        print(e)
+
+for r in n:
+    for e in r[3]:
+        print(e)
