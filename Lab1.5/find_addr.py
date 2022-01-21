e5bf3939c43ad7214b11ea1139dbda853bedd335
Zin-Uru <sacro.uni@gmail.com>
2022-01-21 18:56:30 +0300

    Classificator + regular matches

--- Lab1.5/find_addr.py
+++ Lab1.5/find_addr.py
@@ -2,17 +2,38 @@
 
 import glob
 import ipaddress
+import re
+
+
+class Getinfo:
+    def __init__(self, line):
+        self.line = line.strip().rstrip()
+
+    def mkdict(self):
+        if bool(re.match('^ip address( ([0-9]{1,3}\.){3}[0-9]{1,3}){2}$', self.line)):
+            return {"ip": ipaddress.IPv4Interface((self.line.split(" ")[2], self.line.split(" ")[3]))}
+        elif bool(re.match('^interface ([a-zA-Z0-9\.\-\/]+){1}$', self.line)):
+            return {"int": self.line.split(" ")[1]}
+        elif bool(re.match('^hostname ([a-zA-Z0-9\.\-]+){1}$', self.line)):
+            return {"host": self.line.split(" ")[1]}
+        else:
+            return {}
+
 
 folder = "/home/zinuru/PycharmProjects/pythonProject/Lab1.5/config_files/"
-pat = "ip address"
+# pat = "ip address"
 
 flist = glob.iglob(folder + "*.txt")
 
-result = []
+# result = []
+ips = []
+ints = []
+hosts = []
 
 for i in flist:
     with open(i) as file:
         for j in file.readlines():
+            """
             if pat in j:
                 splitted = j.strip().rstrip().split(" ")
                 try:
@@ -21,8 +42,35 @@ for i in flist:
                     result += [j, ]
                 except (Exception, ):
                     pass
+            """
+            result = Getinfo(j).mkdict()
+            if result == {}:
+                pass
+            elif list(result)[0] == 'ip':
+                ips += [result['ip'], ]
+            elif list(result)[0] == 'int':
+                ints += [result['int'], ]
+            elif list(result)[0] == 'host':
+                hosts += [result['host'], ]
 
+"""
 result = list(set(result))
 
 for i in result:
     print(i, end="")
+"""
+
+print("IP addresses:")
+for i in ips:
+    print(i, end=", ")
+print("\n")
+
+print("Interfaces:")
+for i in ints:
+    print(i, end=", ")
+print("\n")
+
+print("Hosts:")
+for i in hosts:
+    print(i, end=", ")
+print("\n")
